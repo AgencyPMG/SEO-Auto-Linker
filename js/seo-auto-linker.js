2a0697050e2f939c5c0087ef36af48e5e3fa2b79
Christopher Davis <cdavis9999@gmail.com>
2012-05-03 17:07:08 -0500

    Removing JS and CSS -- no longer needed

--- js/seo-auto-linker.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-    var field = jQuery('tr.new-link').html();
-    jQuery('a#pmg-add-more-rows').click(function(){
-        jQuery( 'table#links-definitions tbody').append('<tr class="links-entry">' + field + '</tr>' );
-    });
-    jQuery('a.pmg-delete-row').click(function(){
-        jQuery(this).parent('td').parent('tr').remove();
-    });
-});
