142a3bbddb4307716b6a974754bc03eb8d4cf9e2
Christopher Davis <cdavis9999@gmail.com>
2012-05-03 18:47:03 -0500

    Working front end with the post type container

--- inc/front.php
+++ inc/front.php
@@ -1,192 +1,243 @@
 <?php
-class pmgSeoAutoLinkerFront
+class SEO_Auto_Linker_Front extends SEO_Auto_Linker_Base
 {
-    function __construct()
+    /*
+     * Container for our post autolinker posts
+     */
+    protected static $links;
+
+    /*
+     * Container for our options
+     */
+    protected static $opts;
+
+    /*
+     * Container for the current post's permalink
+     */
+    protected static $permalink;
+
+    /*
+     * Adds actions and filters and such
+     *
+     * @since 0.7
+     */
+    public static function init()
     {
-        add_filter( 'the_content', array( &$this, 'content' ), 1, 1 );
+        add_filter(
+            'the_content',
+            array(get_class(), 'content')
+        );
     }
-    
-    function content( $content )
+
+    /*
+     * Main event.  Filters the conntent to add links
+     *
+     * @since 0.7
+     */
+    public static function content($content)
     {
-        if( ! is_singular() ) return $content;
         global $post;
-        
-        // set up some more options
-        $opts = get_option( 'pmg_autolinker_options' );
-        
-        $permalink = get_permalink( $post );
-        $site_wide_exclude_urls = isset( $opts['site_wide_blacklist'] ) ?
-                        explode( ',', $opts['site_wide_blacklist'] ) : array();
-        if( in_array( $permalink, $site_wide_exclude_urls ) )
-        {
-            return $content;
-        }
-        
-        $kws = isset( $opts['kw'] ) ? (array) $opts['kw'] : array();
-        if( empty( $kws ) ) return $content;
-        
-        // Find all of our <h> tags in the content and replace them with something
+        if(!self::allowed($post)) return $content;
+
+        // we're going to do a lot of counting...
         $counter = 0;
-        $headers_replacements = array();
-        preg_match_all( '/<h[1-6][^>]*>.+?<\/h[1-6]>/iu', $content, $headers );
-        if( ! empty( $headers[0] ) )
-        {
-            foreach( $headers[0] as $h )
-            {
-                $headers_replacements["<!--seo-auto-links-header-{$counter}--!>"] = $h;
-                $counter++;
-            }
-            $filtered_content = str_replace(
-                array_values( $headers_replacements ),
-                array_keys( $headers_replacements ),
-                $content
-            );
-        }
-        else
-        {
-            $filtered_content = $content;
-        }
 
-        $sc_counter = 0;
-        $sc_replacements = array();
-        $sc_regex = get_shortcode_regex();
-        preg_match_all( '/' . $sc_regex . '/', $filtered_content, $shortcodes );
-        if( ! empty( $shortcodes[0] )  )
-        {
-            foreach( $shortcodes[0] as $sc )
-            {
-                $sc_replacements["<!--seo-auto-links-sc-{$sc_counter}-->"]= $sc;
-                $sc_counter++;
-            }
-            $filtered_content = str_replace(
-                array_values( $sc_replacements ),
-                array_keys( $sc_replacements ),
-                $filtered_content
-            );
-        }
-        
-        // Strip out out images, inputs and stuff which may have text
-        // text we shouldn't overwrite
-        $other_counter = 0;
+        $header_replacements = array();
+        $link_replacements = array();
         $other_replacements = array();
-        preg_match_all( '/<(img|input)(.*?) \/?>/iu', $filtered_content, $others );
-        if( ! empty( $others[0] ) )
+        $shortcode_replacements = array();
+        $filtered = $content;
+
+        preg_match_all('/<h[1-6][^>]*>.+?<\/h[1-6]>/iu', $filtered, $headers);
+        if(!empty($headers[0]))
         {
-            foreach( $others[0] as $i )
-            {
-                $other_replacements["<!--seo-auto-links-others-{$other_counter}-->"] = $i;
-                $other_counter++;
-            }
-            $filtered_content = str_replace(
-                array_values( $other_replacements ),
-                array_keys( $other_replacements ), 
-                $filtered_content 
-            );
+            $header_replacements = self::gen_replacements($headers[0], 'header');
+            $filtered = self::replace($header_replacements, $filtered);
         }
 
-        $link_counter = 0;
-        $links_replacements = array();
-        foreach( $kws as $index => $kw )
+        preg_match_all('/' . get_shortcode_regex() . '/', $filtered, $scodes);
+        if(!empty($scodes[0]))
         {
-            $exclude_urls = isset( $opts['blacklist'][$index] ) ? 
-                            explode(',', $opts['blacklist'][$index]) : array();
-            if( in_array( $permalink, $exclude_urls ) ) continue;
+            $shortcode_replacements = self::gen_replacements($scodes[0], 'shortcode');
+            $filtered = self::replace($shortcode_replacements, $filtered);
+        }
 
-            $nope = isset( $opts['types'][$index] ) && $post->post_type != $opts['types'][$index] ? true : false;
-            if( $nope ) continue;
-            
-            $url = isset( $opts['url'][$index] ) ? $opts['url'][$index] : false;
-            if( ! $url || $url == $permalink ) continue;
-            
-            $max = isset( $opts['max'][$index] ) ? $opts['max'][$index] : 1;
-            
-            // Find all the links in the content so we don't overwrite them or get weird stuff
-            preg_match_all( '/<a(.*?)href="(.*?)"(.*?)>(.*?)<\/a>/iu', $filtered_content, $links );
-            if( ! empty( $links[0] ) )
+        preg_match_all('/<(img|input)(.*?) \/?>/iu', $filtered, $others);
+        if(!empty($others[0]))
+        {
+            $other_replacements = self::gen_replacements($others[0], 'others');
+            $filtered = self::replace($other_replacements, $filtered);
+        }
+
+        foreach(self::$links as $l)
+        {
+            preg_match_all(
+                '/<a(.*?)href="(.*?)"(.*?)>(.*?)<\/a>/iu',
+                $filtered,
+                $links
+            );
+            if(!empty($links[0]))
             {
-                $temp_links = array();
-                foreach( $links[0] as $l )
-                {
-                    $temp_links["<!--seo-auto-links-link-{$link_counter}--!>"] = $l;
-                    $link_counter++;
-                }
-                $filtered_content = str_replace( 
-                    array_values( $temp_links ),
-                    array_keys( $temp_links ),
-                    $filtered_content
+                $start = count($link_replacements);
+                $tmp = self::gen_replacements($links[0], 'links', $start);
+                $filtered = self::replace($tmp, $filtered);
+                $link_replacements = array_merge(
+                    $link_replacements,
+                    $tmp
                 );
-                $links_replacements = array_merge( $links_replacements, $temp_links );
             }
-            
-            // Finally! add our links via preg_replace
-            $regex = implode( 
-                '|',
-                array_map(
-                    'preg_quote',
-                    array_map( 
-                        'esc_attr', 
-                        array_map( 
-                            'trim', 
-                            explode( ',', $kw ) 
-                        ) 
-                    )
-                )
-            );
-            $filtered_content = preg_replace( 
-                '/(\b)(' . $regex . ')(\b)/iu',
+
+            $regex = self::get_kw_regex($l);
+            $url = self::get_link_url($l);
+            $max = self::get_link_max($l);
+            $filtered = preg_replace(
+                $regex,
                 '$1<a href="' . esc_url( $url ) . '" title="$2">$2</a>$3',
-                $filtered_content,
-                absint( $max )
-            );
-        }
-        
-        // Put the original <h> tags back in
-        if( ! empty( $headers_replacements ) )
-        {
-            $filtered_content = str_replace(
-                array_keys( $headers_replacements ),
-                array_values( $headers_replacements ),
-                $filtered_content
-            );
-        }
-        
-        // Put links back in
-        if( ! empty( $links_replacements ) )
-        {
-            $filtered_content = str_replace(
-                array_keys( $links_replacements ),
-                array_values( $links_replacements ), 
-                $filtered_content
+                $filtered,
+                $max
             );
         }
 
-        if( ! empty( $sc_replacements ) )
-        {
-            $filtered_content = str_replace(
-                array_keys( $sc_replacements ),
-                array_values( $sc_replacements ),
-                $filtered_content
-            );
-        }
+        $filtered = self::replace_bak($link_replacements, $filtered);
+        $filtered = self::replace_bak($header_replacements, $filtered);
+        $filtered = self::replace_bak($shortcode_replacements, $filtered);
+        $filtered = self::replace_bak($other_replacements, $filtered);
         
-        // Put other stuff back in
-        if( ! empty( $other_replacements ) )
+        return $filtered;
+    }
+
+    /*
+     * Determins whether or not a post can be editted
+     */
+    protected static function allowed($post)
+    {
+        $rv = true;
+        if(!is_singular() || !in_the_loop()) $rv = false;
+
+        self::setup_links($post);
+        if(!self::$links) $rv = false;
+
+        if(in_array(self::$permalink, self::$opts['blacklist'])) $rv = false;
+
+        return apply_filters('seoal_allowed', $rv, $post);
+    }
+
+    /*
+     * Fetch all of the links posts
+     *
+     * @since 0.7
+     */
+    protected static function setup_links($post)
+    {
+        self::$opts = get_option(self::SETTING, array());
+        if(!isset(self::$opts['blacklist'])) self::$opts['blacklist'] = array();
+        self::$permalink = get_permalink($post);
+        $links = get_posts(array(
+            'post_type'   => self::POST_TYPE,
+            'numberposts' => -1,
+            'meta_query'  => array(
+                array(
+                    'key'     => self::get_key("type_{$post->post_type}"),
+                    'value'   => 'on',
+                    'compare' => '='
+                )
+            )
+        ));
+        $rv = array();
+        foreach($links as $l)
         {
-            $filtered_content = str_replace(
-                array_keys( $other_replacements ),
-                array_values( $other_replacements ),
-                $filtered_content
-            );
+            $blacklist = get_post_meta($l->ID, self::get_key('blacklist'), true);
+            if(!$blacklist || !in_array(self::$permalink, $blacklist))
+                $rv[] = $l;
         }
-        
-        $filtered_content = apply_filters(
-            'pmg_seo_auto_linker_content',
-            $filtered_content,
+        self::$links = $rv;
+    }
+
+    /*
+     * Get the regex for a link
+     *
+     * @since 0.7
+     */
+    protected static function get_kw_regex($link)
+    {
+        $keywords = self::get_keywords($link->ID);
+        return sprintf('#(\b)(%s)(\b)#ui', implode('|', $keywords));
+    }
+
+    /*
+     * fetch the clean and sanitied keywords
+     *
+     * @since 0.7
+     */
+    protected static function get_keywords($link_id)
+    {
+        $keywords = get_post_meta($link_id, self::get_key('keywords'), true);
+        $kw_arr = explode(',', $keywords);
+        $kw_arr = array_map('trim', $kw_arr);
+        $kw_arr = array_map('preg_quote', $kw_arr);
+        return $kw_arr;
+    }
+
+    /*
+     * Get the link URL fro a keyword
+     *
+     * @since 0.7
+     */
+    protected static function get_link_url($link)
+    {
+        return get_post_meta($link->ID, self::get_key('url'), true);
+    }
+
+    /*
+     * Get the maximum number of time a link can be replaced
+     *
+     * @since 0.7
+     */
+    protected static function get_link_max($link)
+    {
+        $meta = get_post_meta($link->ID, self::get_key('times'), true);
+        return absint($meta) ? absint($meta) : 1;
+    }
+
+    /*
+     * Loop through a an array of matches and create an associative array of 
+     * key value pairs to use for str replacements
+     *
+     * @since 0.7
+     */
+    protected function gen_replacements($arr, $key, $start=0)
+    {
+        $rv = array();
+        foreach($arr as $a)
+        {
+            $rv["<!--seo-auto-linker-{$key}-{$start}-->"] = $a;
+            $start++;
+        }
+        return $rv;
+    }
+
+    /*
+     * Wrapper around str_replace
+     *
+     * @since 0.7
+     */
+    protected static function replace($arr, $content)
+    {
+        return str_replace(
+            array_values($arr),
+            array_keys($arr),
             $content
         );
-        
-        return $filtered_content;
     }
-} // end class;
 
-new pmgSeoAutoLinkerFront();
+    protected static function replace_bak($arr, $content)
+    {
+        return str_replace(
+            array_keys($arr),
+            array_values($arr),
+            $content
+        );
+    }
+}
+
+SEO_Auto_Linker_Front::init();
