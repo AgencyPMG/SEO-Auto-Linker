ad3e4cfeaed359d46f4edff32b6d377a498eee4f
Christopher Davis <cdavis9999@gmail.com>
2012-05-03 15:31:45 -0500

    No spaces before/after parens

--- inc/admin.php
+++ inc/admin.php
@@ -5,89 +5,89 @@ class pmgSeoAutoLinkerAdmin
     
     function __construct()
     {
-        add_action( 'admin_menu', array( &$this, 'admin_menu' ) );
-        add_action( 'admin_init', array( &$this, 'register_setting' ) );
-        add_action( 'edit_post', array( &$this, 'flush_cache' ) );
+        add_action('admin_menu', array(&$this, 'admin_menu'));
+        add_action('admin_init', array(&$this, 'register_setting'));
+        add_action('edit_post', array(&$this, 'flush_cache'));
     }
     
     function admin_menu()
     {
-        $page = add_options_page( __( 'SEO Auto Linker' ), __( 'SEO Auto Linker' ), 'manage_options', 'pmg-autolinker', array( &$this, 'menu_page' ) );
+        $page = add_options_page(__('SEO Auto Linker'), __('SEO Auto Linker'), 'manage_options', 'pmg-autolinker', array(&$this, 'menu_page'));
         
-        add_action( "admin_print_scripts-{$page}", array( &$this, 'scripts' ) );
-        add_action( "admin_print_styles-{$page}", array( &$this, 'styles' ) );
+        add_action("admin_print_scripts-{$page}", array(&$this, 'scripts'));
+        add_action("admin_print_styles-{$page}", array(&$this, 'styles'));
     }
     
     function menu_page()
     {   
-        $opts = get_option( $this->setting );
+        $opts = get_option($this->setting);
         
-        $keywords = isset( $opts['kw'] ) ? $opts['kw'] : array();
-        $urls = isset( $opts['url'] ) ? $opts['url'] : array();
-        $max_links = isset( $opts['max'] ) ? $opts['max'] : array();
-        $types = isset( $opts['types'] ) ? $opts['types'] : array();
-        $blacklist = isset( $opts['blacklist'] ) ? $opts['blacklist'] : array();
+        $keywords = isset($opts['kw']) ? $opts['kw'] : array();
+        $urls = isset($opts['url']) ? $opts['url'] : array();
+        $max_links = isset($opts['max']) ? $opts['max'] : array();
+        $types = isset($opts['types']) ? $opts['types'] : array();
+        $blacklist = isset($opts['blacklist']) ? $opts['blacklist'] : array();
         
-        $site_wide_blacklist = isset( $opts['site_wide_blacklist'] ) ? $opts['site_wide_blacklist'] : '';
+        $site_wide_blacklist = isset($opts['site_wide_blacklist']) ? $opts['site_wide_blacklist'] : '';
         ?>
         <div class="wrap">
-            <?php screen_icon( 'tools' ); ?>
-            <h2><?php _e( 'SEO Auto Linker Settings' ); ?></h2>
-            <form action="<?php echo admin_url( 'options.php' ); ?>" method="post">
-                <?php settings_fields( $this->setting ); ?>
+            <?php screen_icon('tools'); ?>
+            <h2><?php _e('SEO Auto Linker Settings'); ?></h2>
+            <form action="<?php echo admin_url('options.php'); ?>" method="post">
+                <?php settings_fields($this->setting); ?>
                 <table id="links-definitions" class="widefat">
                     <thead>
                         <tr>
                             <th class="pmg-kw">
-                                <?php _e( 'Comma Separated Keywords' ); ?>
+                                <?php _e('Comma Separated Keywords'); ?>
                             </th>
                             <th class="pmg-url">
-                                <?php _e( 'URL' ); ?>
+                                <?php _e('URL'); ?>
                             </th>
                             <th>
-                                <?php _e( 'Links per Page' ); ?>
+                                <?php _e('Links per Page'); ?>
                             </th>
                             <th>
-                                <?php _e( 'Post Types' ) ; ?>
+                                <?php _e('Post Types') ; ?>
                             </th>
                             <th></th>
                         </tr>
                     </thead>
                     
                     <tbody id="link-definition-body">
-                        <?php foreach( $keywords as $index => $kw ): ?>
+                        <?php foreach($keywords as $index => $kw): ?>
                             <tr class="links-entry">
                                 <td>
-                                    <textarea name="<?php echo $this->setting; ?>[kw][<?php echo $index; ?>]"><?php echo esc_attr( $kw ); ?></textarea>
+                                    <textarea name="<?php echo $this->setting; ?>[kw][<?php echo $index; ?>]"><?php echo esc_attr($kw); ?></textarea>
                                 </td>
                                 
                                 <td>
-                                    <label for="pmg-linksto-<?php echo $index; ?>"><?php _e( 'Link to...' ); ?></label>
-                                    <input type="text" name="<?php echo $this->setting; ?>[url][<?php echo $index; ?>]" id="pmg-linksto-<?php echo $index; ?>" class="pmg-link-text" value="<?php echo $this->get_value( $index, $urls ); ?>" />
+                                    <label for="pmg-linksto-<?php echo $index; ?>"><?php _e('Link to...'); ?></label>
+                                    <input type="text" name="<?php echo $this->setting; ?>[url][<?php echo $index; ?>]" id="pmg-linksto-<?php echo $index; ?>" class="pmg-link-text" value="<?php echo $this->get_value($index, $urls); ?>" />
                                     <br clear="both" />
-                                    <label for="pgm-blacklist-<?php echo $index; ?>"><?php _e( 'Comma Separated Blacklist URLs...' ); ?></label>
-                                    <textarea id="pgm-blacklist-<?php echo $index; ?>" name="<?php echo $this->setting; ?>[blacklist][<?php echo $index; ?>]"><?php echo $this->get_value( $index, $blacklist ); ?></textarea>
+                                    <label for="pgm-blacklist-<?php echo $index; ?>"><?php _e('Comma Separated Blacklist URLs...'); ?></label>
+                                    <textarea id="pgm-blacklist-<?php echo $index; ?>" name="<?php echo $this->setting; ?>[blacklist][<?php echo $index; ?>]"><?php echo $this->get_value($index, $blacklist); ?></textarea>
                                 </td>
                                 
                                 <td>
-                                    <?php _e( 'Up to ' ); ?>
+                                    <?php _e('Up to '); ?>
                                     <select name="<?php echo $this->setting; ?>[max][<?php echo $index; ?>]">
                                         <?php
-                                            foreach( range( 1, 5 ) as $num )
-                                                echo '<option value="' . $num . '" ' . selected( $num, $this->get_value( $index, $max_links ), false ) . '">' . $num . '</option>';
+                                            foreach(range(1, 5) as $num)
+                                                echo '<option value="' . $num . '" ' . selected($num, $this->get_value($index, $max_links), false) . '">' . $num . '</option>';
                                         ?>
                                     </select>
-                                    <?php _e( ' times on each...' ); ?>
+                                    <?php _e(' times on each...'); ?>
                                 </td>
                                 <td>
                                     <select name="<?php echo $this->setting; ?>[types][<?php echo $index; ?>]">
                                         <?php
-                                            foreach( get_post_types() as $type )
+                                            foreach(get_post_types() as $type)
                                             {
-                                                if( in_array( $type, array( 'nav_menu_item', 'revision', 'attachment' ) ) ) continue;
-                                                $typeobj = get_post_type_object( $type );
-                                                $name = isset( $typeobj->labels->singular_name ) ? $typeobj->labels->singular_name : $typeobj->label;
-                                                echo '<option value="' . $type . '" ' . selected( $type, $this->get_value( $index, $types ), false ) . '>' . esc_attr( $name ) . '</option>';
+                                                if(in_array($type, array('nav_menu_item', 'revision', 'attachment'))) continue;
+                                                $typeobj = get_post_type_object($type);
+                                                $name = isset($typeobj->labels->singular_name) ? $typeobj->labels->singular_name : $typeobj->label;
+                                                echo '<option value="' . $type . '" ' . selected($type, $this->get_value($index, $types), false) . '>' . esc_attr($name) . '</option>';
                                             }
                                         ?>
                                     </select>
@@ -104,30 +104,30 @@ class pmgSeoAutoLinkerAdmin
                                 <textarea name="<?php echo $this->setting; ?>[kw][]"></textarea>
                             </td>
                             <td>
-                                <label for="pmg-linksto"><?php _e( 'Link to...' ); ?></label>
+                                <label for="pmg-linksto"><?php _e('Link to...'); ?></label>
                                 <input type="text" name="<?php echo $this->setting; ?>[url][]" id="pmg-linksto" class="pmg-link-text" />
-                                <label for="pgm-blacklist"><?php _e( 'Comma Separated Blacklist URLs…' ); ?></label>
+                                <label for="pgm-blacklist"><?php _e('Comma Separated Blacklist URLs…'); ?></label>
                                     <textarea id="pgm-blacklist" name="<?php echo $this->setting; ?>[blacklist][]"></textarea>
                             </td>
                             <td>
-                                <?php _e( 'Up to ' ); ?>
+                                <?php _e('Up to '); ?>
                                 <select name="<?php echo $this->setting; ?>[max][]">
                                     <?php
-                                        foreach( range( 1, 5 ) as $num )
+                                        foreach(range(1, 5) as $num)
                                             echo '<option value="' . $num . '">' . $num . '</option>';
                                     ?>
                                 </select>
-                                <?php _e( ' times on each... ' ); ?>
+                                <?php _e(' times on each... '); ?>
                             </td>
                             <td>
                                 <select name="<?php echo $this->setting; ?>[types][]">
                                 <?php
-                                    foreach( get_post_types() as $type )
+                                    foreach(get_post_types() as $type)
                                     {
-                                        if( in_array( $type, array( 'nav_menu_item', 'revision', 'attachment' ) ) ) continue;
-                                        $typeobj = get_post_type_object( $type );
-                                        $name = isset( $typeobj->labels->singular_name ) ? $typeobj->labels->singular_name : $typeobj->label;
-                                        echo '<option value="' . $type . '">' . esc_attr( $name ) . '</option>';
+                                        if(in_array($type, array('nav_menu_item', 'revision', 'attachment'))) continue;
+                                        $typeobj = get_post_type_object($type);
+                                        $name = isset($typeobj->labels->singular_name) ? $typeobj->labels->singular_name : $typeobj->label;
+                                        echo '<option value="' . $type . '">' . esc_attr($name) . '</option>';
                                     }
                                 ?>
                                 </select>
@@ -137,7 +137,7 @@ class pmgSeoAutoLinkerAdmin
                     </tbody>
                     <tfoot>
                         <th colspan="4">
-                            <a href="javascript:void(null)" id="pmg-add-more-rows" class="button-secondary"><?php _e( 'Add More Rows' ); ?></a>
+                            <a href="javascript:void(null)" id="pmg-add-more-rows" class="button-secondary"><?php _e('Add More Rows'); ?></a>
                         </th>
                     </tfoot>
                 </table>
@@ -145,7 +145,7 @@ class pmgSeoAutoLinkerAdmin
                     <thead>
                         <tr>
                             <th>
-                                <?php _e( 'Site Wide Comma Separated Blacklist URLs...' ); ?>
+                                <?php _e('Site Wide Comma Separated Blacklist URLs...'); ?>
                             </th>
                         </tr>
                     </thead>
@@ -160,7 +160,7 @@ class pmgSeoAutoLinkerAdmin
                 </table>
                 
                 <p>
-                    <input type="submit" class="button-primary" value="<?php _e( 'Save Settings' ); ?>" />
+                    <input type="submit" class="button-primary" value="<?php _e('Save Settings'); ?>" />
                 </p>
             </form>
         </div>
@@ -169,45 +169,45 @@ class pmgSeoAutoLinkerAdmin
     
     function register_setting()
     {
-        register_setting( $this->setting, $this->setting, array( &$this, 'clean_settings' ) );
+        register_setting($this->setting, $this->setting, array(&$this, 'clean_settings'));
     }
     
-    function clean_settings( $in )
+    function clean_settings($in)
     {
         $out = array();
         $out['site_wide_blacklist'] = esc_attr($in['site_wide_blacklist']);
-        if( isset( $in['kw'] ) ):
-            foreach( $in['kw'] as $index => $kw )
+        if(isset($in['kw'])):
+            foreach($in['kw'] as $index => $kw)
             {
-                if( ! $kw ) continue;
-                $out['kw'][$index] = esc_attr( $kw );
-                $out['url'][$index] = isset( $in['url'][$index] ) && $in['url'][$index] ? esc_url( $in['url'][$index] ) : esc_url( home_url() );
-                $out['max'][$index] = isset( $in['max'][$index] ) ? absint( $in['max'][$index] ) : 1;
-                $out['types'][$index] = isset( $in['types'][$index] ) ? esc_attr( $in['types'][$index] ) : 'post';
-                $out['blacklist'][$index] = esc_attr( $in['blacklist'][$index] );
+                if(! $kw) continue;
+                $out['kw'][$index] = esc_attr($kw);
+                $out['url'][$index] = isset($in['url'][$index]) && $in['url'][$index] ? esc_url($in['url'][$index]) : esc_url(home_url());
+                $out['max'][$index] = isset($in['max'][$index]) ? absint($in['max'][$index]) : 1;
+                $out['types'][$index] = isset($in['types'][$index]) ? esc_attr($in['types'][$index]) : 'post';
+                $out['blacklist'][$index] = esc_attr($in['blacklist'][$index]);
             }
         endif;
         return $out;
     }
     
-    function flush_cache( $post_id )
+    function flush_cache($post_id)
     {
-        wp_cache_delete( 'autolinker_content_' . $post_id );
+        wp_cache_delete('autolinker_content_' . $post_id);
     }
     
     function styles()
     {   
-        wp_enqueue_style( 'pmg-auto-linker', SEOAL_URL . 'css/seo-auto-linker.css', array(), NULL, 'all' );
+        wp_enqueue_style('pmg-auto-linker', SEOAL_URL . 'css/seo-auto-linker.css', array(), NULL, 'all');
     }
     
     function scripts()
     {
-        wp_enqueue_script( 'pmg-auto-linkers', SEOAL_URL . 'js/seo-auto-linker.js', array( 'jquery' ), NULL, true );
+        wp_enqueue_script('pmg-auto-linkers', SEOAL_URL . 'js/seo-auto-linker.js', array('jquery'), NULL, true);
     }
     
-    function get_value( $index, $array )
+    function get_value($index, $array)
     {
-        if( isset( $array[$index] ) ) return esc_attr( $array[$index] );
+        if(isset($array[$index])) return esc_attr($array[$index]);
     }
 } // end class
 
--- seo-auto-linker.php
+++ seo-auto-linker.php
@@ -3,19 +3,21 @@
 Plugin Name: SEO Auto Linker
 Plugin URI: http://pmg.co/seo-auto-linker
 Description: Allows you to automatically link terms with in your post, page or custom post type content.
-Version: 0.6.5
+Version: 0.7
 Author: Christopher Davis at PMG
 Author URI: http://christopherdavis.me
 */
 
-define( 'SEOAL_PATH', plugin_dir_path( __FILE__ ) );
-define( 'SEOAL_URL', plugin_dir_url( __FILE__ ) );
+define('SEOAL_PATH', plugin_dir_path(__FILE__));
+define('SEOAL_URL', plugin_dir_url(__FILE__));
 
-if( is_admin() )
+require_once(SEOAL_PATH . 'inc/base.php');
+require_once(SEOAL_PATH . 'inc/post-type.php');
+if(is_admin())
 {
-    require_once( SEOAL_PATH . 'inc/admin.php' );
+    require_once(SEOAL_PATH . 'inc/admin.php');
 }
 else
 {
-    require_once( SEOAL_PATH . 'inc/front.php' );
+    require_once(SEOAL_PATH . 'inc/front.php');
 }
