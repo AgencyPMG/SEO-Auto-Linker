1a8339c434b64873f7cb5e4fe1da4d83b541742a
Christopher Davis <cdavis9999@gmail.com>
2012-05-04 12:42:21 -0500

    Adding license notification and textdomain stuff to file header

--- seo-auto-linker.php
+++ seo-auto-linker.php
@@ -6,6 +6,24 @@ Description: Allows you to automatically link terms with in your post, page or c
 Version: 0.7
 Author: Christopher Davis at PMG
 Author URI: http://christopherdavis.me
+Text Domain: seoal
+Domain Path: /lang
+*/
+/*
+    Copyright 2012 Christopher Davis
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 define('SEOAL_PATH', plugin_dir_path(__FILE__));
